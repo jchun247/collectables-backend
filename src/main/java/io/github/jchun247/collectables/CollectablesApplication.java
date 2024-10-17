863855d36c6502c721661c1f22fb665cf4a18819
Justin Chung <jchung1573@gmail.com>
2024-10-17 10:42:16 -0400

    initial commit

---
+++ src/main/java/io/github/jchun247/collectables/CollectablesApplication.java
@@ -0,0 +1,13 @@
+package io.github.jchun247.collectables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectablesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jchun247/collectables/CollectablesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jchun247.collectables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
