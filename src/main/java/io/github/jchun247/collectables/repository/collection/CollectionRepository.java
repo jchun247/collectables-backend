bea0624fd7fd92a98fe056e935ced45a5d4742cd
Justin Chung <jchun247@uwo.ca>
2025-02-19 15:24:38 -0500

    Removed legacy collection and collectionCard files

--- src/main/java/io/github/jchun247/collectables/repository/collection/CollectionRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.jchun247.collectables.repository.collection;
-
-import io.github.jchun247.collectables.model.collection.Collection;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CollectionRepository extends JpaRepository<Collection, Long> {
-}
