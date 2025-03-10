d19a0e192f7664c460ffb086f78b7d217db34f9b
Justin Chung <jchun247@uwo.ca>
2025-03-10 14:24:23 -0400

    Add external_id field to Card for card price import process

--- src/main/java/io/github/jchun247/collectables/model/card/Card.java
+++ src/main/java/io/github/jchun247/collectables/model/card/Card.java
@@ -19,6 +19,8 @@ public class Card {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String externalId;
     private String name;
 
     @ManyToOne
