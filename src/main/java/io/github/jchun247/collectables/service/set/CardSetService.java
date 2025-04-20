6cfb6196b77917cff1d3d3e2ae2d93313b949885
Justin Chung <jchun247@uwo.ca>
2025-04-19 22:36:23 -0400

    Initial card set changes

--- src/main/java/io/github/jchun247/collectables/service/set/CardSetService.java
+++ src/main/java/io/github/jchun247/collectables/service/set/CardSetService.java
@@ -1,11 +1,12 @@
 package io.github.jchun247.collectables.service.set;
 
+import io.github.jchun247.collectables.dto.card.BasicCardSetDTO;
 import io.github.jchun247.collectables.model.card.CardSeries;
 import io.github.jchun247.collectables.model.card.CardSet;
 
 import java.util.List;
 
 public interface CardSetService {
-    List<CardSet> getCardSetsBySeries(CardSeries series);
+    List<BasicCardSetDTO> getCardSetsBySeries(CardSeries series);
     List<CardSeries> getAllCardSeries();
 }
