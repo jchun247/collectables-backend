b285828b71cf6fa731a40ded7cfa6cd85bc9575a
Justin Chung <jchun247@uwo.ca>
2025-06-21 00:15:53 -0400

    Remove commented out code

--- src/main/java/io/github/jchun247/collectables/repository/card/CardRepository.java
+++ src/main/java/io/github/jchun247/collectables/repository/card/CardRepository.java
@@ -15,56 +15,12 @@ import java.util.Optional;
 import java.util.Set;
 
 public interface CardRepository extends JpaRepository<Card, Long>, CardRepositoryCustom {
-//    @Query(value = "SELECT DISTINCT c.id FROM Card c " +
-//            "JOIN c.set cs " +
-//            "JOIN c.prices p " +
-//            "WHERE (:games IS NULL OR c.game IN :games) " +
-//            "AND (:setId IS NULL OR cs.id = :setId) " +
-//            "AND (:rarity IS NULL OR c.rarity = :rarity) " +
-//            "AND (LOWER(c.name) LIKE LOWER(CONCAT('%', :query, '%')) OR :query IS NULL OR cs.id LIKE %:query% OR c.setNumber LIKE %:query%) " +
-//            "AND p.condition = :condition " +
-//            "AND (:finish IS NULL OR p.finish = :finish) " +
-//            "AND p.price BETWEEN :minPrice AND :maxPrice")
-//    Page<Long> findCardIds(
-//            @Param("games") List<CardGame> games,
-//            @Param("setId") String setId,
-//            @Param("rarity") CardRarity rarity,
-//            @Param("condition") CardCondition condition,
-//            @Param("finish") CardFinish finish,
-//            @Param("query") String query,
-//            @Param("minPrice") BigDecimal minPrice,
-//            @Param("maxPrice") BigDecimal maxPrice,
-//            Pageable pageable
-//    );
-
-//    @Query(value = "SELECT c.id FROM Card c " +
-//            "JOIN c.set cs " +
-//            "JOIN c.prices p " +
-//            "WHERE (:games IS NULL OR c.game IN :games) " +
-//            "AND (:setId IS NULL OR cs.id = :setId) " +
-//            "AND (:rarity IS NULL OR c.rarity = :rarity) " +
-//            "AND (LOWER(c.name) LIKE LOWER(CONCAT('%', :query, '%')) OR :query IS NULL OR cs.id LIKE %:query% OR c.setNumber LIKE %:query%) " +
-//            "AND p.condition = :condition " +
-//            "AND (:finish IS NULL OR p.finish = :finish) " +
-//            "AND p.price BETWEEN :minPrice AND :maxPrice")
-//    Page<Long> findCardIdsSortedByPrice(
-//            @Param("games") List<CardGame> games,
-//            @Param("setId") String setId,
-//            @Param("rarity") CardRarity rarity,
-//            @Param("condition") CardCondition condition,
-//            @Param("finish") CardFinish finish,
-//            @Param("query") String query,
-//            @Param("minPrice") BigDecimal minPrice,
-//            @Param("maxPrice") BigDecimal maxPrice,
-//            Pageable pageable
-//    );
     /**
      * Takes a list of Card IDs and fetches the full Card objects with their collections.
      */
     @EntityGraph(attributePaths = {"images", "set", "prices", "pokemonDetails"})
     @Query("SELECT c FROM Card c WHERE c.id IN :ids")
     List<Card> findFullCardsByIds(@Param("ids") List<Long> ids);
-
 
     @EntityGraph(attributePaths = {"prices", "images", "set"})
     @Query("SELECT c FROM Card c WHERE c.id = :id")
