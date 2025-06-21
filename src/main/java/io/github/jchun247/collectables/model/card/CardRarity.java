17d65735cf5ce643c5c77da58004d9fd7245d5c1
Justin Chung <jchun247@uwo.ca>
2025-06-21 00:15:19 -0400

    Define sort order in CardRarity class

--- src/main/java/io/github/jchun247/collectables/model/card/CardRarity.java
+++ src/main/java/io/github/jchun247/collectables/model/card/CardRarity.java
@@ -4,46 +4,48 @@ import lombok.Getter;
 
 @Getter
 public enum CardRarity {
-    HYPER_RARE("Hyper Rare"),
-    RARE("Rare"),
-    RARE_HOLO_LVX("Rare Holo LV.X"),
-    SPECIAL_ILLUSTRATION_RARE("Special Illustration Rare"),
-    RARE_HOLO_STAR("Rare Holo Star"),
-    ILLUSTRATION_RARE("Illustration Rare"),
-    SHINY_ULTRA_RARE("Shiny Ultra Rare"),
-    RARE_SHINY("Rare Shiny"),
-    RARE_HOLO_GX("Rare Holo GX"),
-    LEGEND("LEGEND"),
-    CLASSIC_COLLECTION("Classic Collection"),
-    DOUBLE_RARE("Double Rare"),
-    RARE_BREAK("Rare BREAK"),
-    RARE_HOLO_V("Rare Holo V"),
-    RARE_PRISM_STAR("Rare Prism Star"),
-    ACE_SPEC_RARE("ACE SPEC Rare"),
-    RARE_RAINBOW("Rare Rainbow"),
-    COMMON("Common"),
-    RADIANT_RARE("Radiant Rare"),
-    RARE_PRIME("Rare Prime"),
-    RARE_HOLO_VSTAR("Rare Holo VSTAR"),
-    AMAZING_RARE("Amazing Rare"),
-    RARE_HOLO_VMAX("Rare Holo VMAX"),
-    RARE_SECRET("Rare Secret"),
-    RARE_ULTRA("Rare Ultra"),
-    UNCOMMON("Uncommon"),
-    RARE_ACE("Rare ACE"),
-    SHINY_RARE("Shiny Rare"),
-    ULTRA_RARE("Ultra Rare"),
-    RARE_HOLO_EX("Rare Holo EX"),
-    RARE_HOLO("Rare Holo"),
-    RARE_SHINY_GX("Rare Shiny GX"),
-    RARE_SHINING("Rare Shining"),
-    TRAINER_GALLERY_RARE_HOLO("Trainer Gallery Rare Holo"),
-    PROMO("Promo");
+    COMMON("Common", 10),
+    UNCOMMON("Uncommon", 20),
+    RARE("Rare", 30),
+    RARE_PRISM_STAR("Rare Prism Star", 40),
+    RARE_HOLO("Rare Holo", 50),
+    RARE_ULTRA("Rare Ultra", 60),
+    ULTRA_RARE("Ultra Rare", 70),
+    RARE_HOLO_STAR("Rare Holo Star", 80),
+    RARE_HOLO_EX("Rare Holo EX", 90),
+    RARE_HOLO_LVX("Rare Holo LV.X", 100),
+    RARE_PRIME("Rare Prime", 110),
+    LEGEND("LEGEND", 120),
+    RARE_BREAK("Rare BREAK", 130),
+    RARE_HOLO_GX("Rare Holo GX", 140),
+    RARE_HOLO_V("Rare Holo V", 150),
+    RARE_HOLO_VMAX("Rare Holo VMAX", 160),
+    RARE_HOLO_VSTAR("Rare Holo VSTAR", 170),
+    DOUBLE_RARE("Double Rare", 180),
+    AMAZING_RARE("Amazing Rare", 190),
+    RADIANT_RARE("Radiant Rare", 200),
+    RARE_SHINING("Rare Shining", 201),
+    ACE_SPEC_RARE("ACE SPEC Rare", 210),
+    RARE_ACE("Rare ACE", 220),
+    CLASSIC_COLLECTION("Classic Collection", 230),
+    RARE_SHINY("Rare Shiny", 240),
+    SHINY_RARE("Shiny Rare", 250),
+    RARE_SHINY_GX("Rare Shiny GX", 260),
+    TRAINER_GALLERY_RARE_HOLO("Trainer Gallery Rare Holo", 270),
+    ILLUSTRATION_RARE("Illustration Rare", 280),
+    SPECIAL_ILLUSTRATION_RARE("Special Illustration Rare", 290),
+    RARE_SECRET("Rare Secret", 300),
+    RARE_RAINBOW("Rare Rainbow", 310),
+    HYPER_RARE("Hyper Rare", 320),
+    SHINY_ULTRA_RARE("Shiny Ultra Rare", 330),
+    PROMO("Promo", 340);
 
     private final String databaseValue;
+    private final int sortOrder;
 
-    CardRarity(String databaseValue) {
+    CardRarity(String databaseValue, int sortOrder) {
         this.databaseValue = databaseValue;
+        this.sortOrder = sortOrder;
     }
 
     public static CardRarity fromDatabaseValue(String value) {
