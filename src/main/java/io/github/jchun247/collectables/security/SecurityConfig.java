7115084174ba060c57eb53c050d9e8db50f619a6
Justin Chung <jchun247@uwo.ca>
2025-09-02 10:49:26 -0400

    Added transaction-history endpoint to securityFilterChain

--- src/main/java/io/github/jchun247/collectables/security/SecurityConfig.java
+++ src/main/java/io/github/jchun247/collectables/security/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/sets/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/collections/{collectionId}").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/collections/{collectionId}/cards").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/collections/{collectionId}/cards/{collectionCardId}").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/collections/{collectionId}/cards/{collectionCardId}/transaction-history").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/collections/{collectionId}/value-history").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/collections/{collectionId}/value-history/chart").permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/api/users/provision", "POST")).permitAll()
@@ -59,7 +59,7 @@ public class SecurityConfig {
                                 requestURI.startsWith("/api/users/provision") ||
                                     (HttpMethod.GET.matches(request.getMethod()) && (
                                             requestURI.matches("/api/collections/[^/]+/?") || // Base collection endpoint
-                                            requestURI.matches("/api/collections/[^/]+/(?:cards|value-history(?:/chart)?)")))) {
+                                            requestURI.matches("/api/collections/[^/]+/(?:cards(?:/[^/]+/transaction-history)?|value-history(?:/chart)?)")))) {
                                     response.setStatus(HttpServletResponse.SC_OK);
                                     return;
                             }
